82953170d8bb44e02dc27ca9fcb06fd273f543c6
clayzim <clayzim@umich.edu>
2017-09-02 22:13:17 -0400

    Hard-coded a valid test cube to use as an example until I implement user input

--- cube.hs
+++ cube.hs
@@ -11,6 +11,15 @@ type Piece = [Sticker]
 
 type Cube = [Piece]
 
+--Specific cube state to play with, since input is not yet implemented
+exampleCube = [[(Up, Orange), (Front, Green), (Left, Yellow)],[(Front, Yellow), (Up, Orange)],[(Front, Green), (Up, Red), (Right, Yellow)],[(Front, Orange), (Left, Blue)],[(Front, Yellow), (Right, Green)],[(Front, Blue), (Left, Red), (Down, Yellow)],[(Front, Red), (Down, Green)],[(Front, Blue), (Right, Orange), (Down, Yellow)],[(Right, Blue), (Up, Red)],[(Right, White), (Down, Red)],[(Right, Green), (Back, White), (Up, Orange)],[(Right, Blue), (Back, White)],[(Right, Blue), (Back, White), (Down, Orange)],[(Back, Orange), (Up, Green)],[(Back, Blue), (Down, Yellow)],[(Back, White), (Left, Green), (Up, Red)],[(Back, Orange), (Left, White)],[(Back, White), (Left, Blue), (Down, Red)],[(Left, Yellow), (Up, Red)],[(Left, Green), (Down, White)]]
+
+isEdge :: Piece -> Bool
+isEdge piece = length piece == 2
+
+isCorner :: Piece -> Bool
+isCorner piece = length piece == 3
+
 hasColor :: Color -> Piece -> Bool
 hasColor thisColor piece = thisColor `elem` colors piece
 
@@ -38,6 +47,7 @@ inFinalPosition piece = all onCorrectFace piece
 onCorrectFace :: Sticker -> Bool
 onCorrectFace sticker = face sticker == appropriateFace sticker
 
+--Hard-coded. Maybe not the best-possible solution
 appropriateFace :: Sticker -> Face
 appropriateFace sticker
     | thisColor == Red = Front
