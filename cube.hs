92e1ac730d16d49949d635bb5923b5eac1df5147
clayzim <clayzim@umich.edu>
2017-09-02 23:48:51 -0400

    Rotation functions implemented. Introduced types for axis & rotation to specify in terms of those properties.

--- cube.hs
+++ cube.hs
@@ -5,6 +5,9 @@ import Data.Maybe
 data Face = Front | Right | Back | Left | Up | Down deriving (Ord, Eq, Enum, Show)
 data Color = Red | Green | Orange | Blue | Yellow | White deriving (Ord, Eq, Enum, Show)
 
+data Axis = X | Y | Z deriving (Ord, Eq, Enum, Show)
+data Rotation = Clockwise | Counterclockwise deriving (Ord, Eq, Enum, Show)
+
 type Sticker = (Face, Color)
 
 type Piece = [Sticker]
@@ -45,11 +48,11 @@ inFinalPosition :: Piece -> Bool
 inFinalPosition piece = all onCorrectFace piece
 
 onCorrectFace :: Sticker -> Bool
-onCorrectFace sticker = face sticker == appropriateFace sticker
+onCorrectFace sticker = face sticker == correctFace sticker
 
 --Hard-coded. Maybe not the best-possible solution
-appropriateFace :: Sticker -> Face
-appropriateFace sticker
+correctFace :: Sticker -> Face
+correctFace sticker
     | thisColor == Red = Front
     | thisColor == Green = Right
     | thisColor == Orange = Back
@@ -57,3 +60,44 @@ appropriateFace sticker
     | thisColor == Yellow = Up
     | thisColor == White = Down
     where thisColor = color sticker
+
+rotateCube :: Face -> Cube -> Cube
+rotateCube face cube = rotated ++ unchanged
+                       where facePieces = filter (hasFace face) cube
+                             rotated = map (rotatePieceByFace face) facePieces
+                             unchanged = filter (`notElem` facePieces) cube
+
+--Potentially change order of axis & rotation arguments
+rotatePiece :: Axis -> Rotation -> (Piece -> Piece)
+rotatePiece axis rotation
+    | rotation == Clockwise = map (nextSticker sequence)
+    | otherwise = map (nextSticker $ reverse sequence)
+    where sequence = case axis of
+                        X -> [Front, Up, Back, Down]
+                        Y -> [Front, Left, Back, Right]
+                        Z -> [Up, Right, Down, Left]
+
+--Face names don't correspond to expected behavior when called outside of rotateCube.
+--The rotation has more to do with axis and clockwise-ness than face.
+rotatePieceByFace :: Face -> (Piece -> Piece)
+rotatePieceByFace face
+    | face == Front = map (nextSticker zClockwise)
+    | face == Right = map (nextSticker xClockwise)
+    | face == Back = map (nextSticker $ reverse zClockwise)
+    | face == Left = map (nextSticker $ reverse xClockwise)
+    | face == Up = map (nextSticker yClockwise)
+    | face == Down = map (nextSticker $ reverse yClockwise)
+    where xClockwise = [Front, Up, Back, Down]
+          yClockwise = [Front, Left, Back, Right]
+          zClockwise = [Up, Right, Down, Left]
+
+nextSticker :: [Face] -> Sticker -> Sticker
+nextSticker sequence (face, color) = (nextInCycle sequence face, color)
+
+--There may well be a cleaner/more concise way to write this
+nextInCycle :: (Eq a) => [a] -> a -> a
+nextInCycle sequence currentValue
+    | isJust currentIndex = thisCycle !! (fromJust currentIndex + 1)
+    | otherwise = currentValue
+    where currentIndex = elemIndex currentValue sequence
+          thisCycle = cycle sequence
